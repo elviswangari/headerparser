1edd46bf591ddd15d55bcac22aa4e8e976f7d3db
elviswangari <95751917+elviswangari@users.noreply.github.com>
2024-01-09 13:49:29 +0000

    whoami

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const os = require('os');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -22,6 +23,15 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip, 
+    language: req.headers['accept-language'],
+    software: os.type() + ' ' + os.release()
+  });
 });
 
 // listen for requests :)
